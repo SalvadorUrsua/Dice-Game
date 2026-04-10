5081d3e8c9a0f44e7d0775886186f0e9cd6ae1ba
Salvador Ursua <118657557+SalvadorUrsua@users.noreply.github.com>
2026-04-10 18:19:47 +0800

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,24 @@
+document.querySelector(".roll").addEventListener("click", () => {
+  const diceImg = [
+    "dice1.png",
+    "dice2.png",
+    "dice3.png",
+    "dice4.png",
+    "dice5.png",
+    "dice6.png",
+  ];
+
+  var diceLeft = diceImg[Math.floor(Math.random() * 6)];
+  document.querySelector(".img1").setAttribute("src", `../images/${diceLeft}`);
+  var diceRight = diceImg[Math.floor(Math.random() * 6)];
+  document.querySelector(".img2").setAttribute("src", `../images/${diceRight}`);
+
+  var flagWinner = document.querySelector("h1");
+  if (diceLeft > diceRight) {
+    flagWinner.textContent = "🚩Player 1 Wins!";
+  } else if (diceLeft < diceRight) {
+    flagWinner.textContent = "Player 2 Wins!🚩";
+  } else {
+    flagWinner.textContent = "It's a Draw!";
+  }
+});
